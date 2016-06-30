5e50c30c5ec67a1d10c0aa0fc423ff85835bf761
yuedongze <dy85@cornell.edu>
2016-06-30 02:31:11 -0400

    parse

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from distutils.core import setup, Extension
 
 extension_mod = Extension("sony", include_dirs = ['/usr/include', '/usr/local/include'] ,libraries = ['usb'], library_dirs = ['/usr/lib', '/usr/local/lib'], sources = ["ptp.c" , "main.c"])
 
-setup(name = "sony", ext_modules=[extension_mod])
+setup(name = "sony", ext_modules=[extension_mod], py_modules=['parser'])
